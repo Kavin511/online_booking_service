4866535f62d48c0d5c6d885fbcc19ace55d84835
kavin511 <kavinalmighty@gmail.com>
2022-06-10 04:43:47 +0530

    Changed procfile

--- routes/booking.js
+++ routes/booking.js
@@ -4,6 +4,7 @@ const {
     booking,
     updateBooking
 } = require('../controller/booking')
+router.get('/',(req,res)=>{res.status(200).json({"message":"Welcome!"})})
 router.post('/bookings', updateBooking)
 router.get('/bookings', booking)
 module.exports = router

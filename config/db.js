22c52b7715e0151301bc520d7895aff37b4450f6
kavin511 <kavinalmighty@gmail.com>
2022-06-10 05:59:47 +0530

    Changed db file

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql')
-const dbConfig = require('./dbconfig');
-
+// const dbConfig = require('./dbconfig');
+require('dotenv').config()
+// module.exports = {
+//     secret: process.env.DB_SECRET,
+//     user: process.env.DB_USER,
+//     host:process.env.DB_HOST,
+//     database:process.env.DATABASE
+// }
 const mysqlConnection = mysql.createConnection({
-    host: dbConfig.host,
-    user: dbConfig.user,
-    password: dbConfig.secret,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_SECRET,
     multipleStatements: true
 });
 mysqlConnection.connect((err) => {
@@ -14,5 +20,5 @@ mysqlConnection.connect((err) => {
         console.log("Connected successfully!")
     }
 })
-mysqlConnection.query("use " + dbConfig.database)
+mysqlConnection.query("use " + process.env.DATABASE)
 module.exports = mysqlConnection
